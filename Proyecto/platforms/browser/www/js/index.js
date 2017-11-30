321f8737666aa33443940fda394ca1951cee6b3a
Nacho <carlos.ralcantar@alumnos.udg.mx>
2017-11-30 17:17:12 -0600

    Script log in funciona, pagina de alumno incluida

--- Proyecto/platforms/browser/www/js/index.js
+++ Proyecto/platforms/browser/www/js/index.js
@@ -3,8 +3,22 @@ function enviar() {
 	var password = $('#pass').val();
 	//alert(usuario+" "+password);
 	$.post("https://dcc.000webhostapp.com/pruebaLogin.php", {codigo: usuario, nip: password}, function(result,status){
-		console.log(result);
+		//
+		if(result == "0")
+			error();
+		else {
+			var tipo = result.substr(0,1);
+			if (tipo == "A") { //Alumno
+				//alert("Fucking alumno");
+				$(location).attr("href","inicioAlumno.html");
+			} if(tipo == "T"){} // Trabajador
+		}
 	});
+}
+
+function error(){
+	$("#lblError").fadeIn(2000);
+	$("#lblError").fadeOut(2000);
 }
 
 $(document).keypress(function(e){
