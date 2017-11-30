70cf9354f98b3572cf28cf9add65bdd5929389a1
Javier <javiermorenot96@gmail.com>
2017-11-29 22:09:31 -0600

    lel

--- www/js/index.js
+++
@@ -1,7 +0,0 @@
-$("#form").on('submit', function(){
-	var usuario = $('#user').val();
-	var password = $('#pass').val();
-	$.post("https://dcc.000webhostapp.com/pruebaLogin.php", {codigo: usuario, nip: password}, function(result,status){
-		console.log(status);
-	});
-});
