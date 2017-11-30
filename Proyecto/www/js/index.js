f1dea1a6adec54e36f10da879e731a4b5679c8c8
Nacho <carlos.ralcantar@alumnos.udg.mx>
2017-11-29 23:37:37 -0600

    Comunicacion con servidor dcc

--- Proyecto/www/js/index.js
+++ Proyecto/www/js/index.js
@@ -1,7 +1,14 @@
-$("#form").on('submit', function(){
+function enviar() {
 	var usuario = $('#user').val();
 	var password = $('#pass').val();
+	//alert(usuario+" "+password);
 	$.post("https://dcc.000webhostapp.com/pruebaLogin.php", {codigo: usuario, nip: password}, function(result,status){
-		console.log(status);
+		console.log(result);
 	});
+}
+
+$(document).keypress(function(e){
+	if(e.which == 13)
+		if($('#user').val() != "" && $('#pass').val() != "")
+			$("#enviar").click();
 });
