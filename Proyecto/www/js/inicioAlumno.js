a300ef04479e3a7eab4134b6de9737004d5ada99
Nacho <carlos.ralcantar@alumnos.udg.mx>
2017-12-01 15:41:39 -0600

    Se agrega script para logeo y se corrigen errores en index

---
+++ Proyecto/www/js/inicioAlumno.js
@@ -0,0 +1,9 @@
+//$("#temp").html('<object data="https://crollz.000webhostapp.com/charts/charts.php">');
+
+//$('#temp').load('http://www.somesitehere.com');
+
+/*$.get('http://a_site.com/a_page.html', function(data) {
+    $('#temp').html(data);
+});*/
+
+document.getElementById("temp").innerHTML='<object type="text/html" data="https://crollz.000webhostapp.com/jsonDate/jsonDate.php" ></object>';
