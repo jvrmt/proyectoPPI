29f083693ddbf9bd1e8ff8be66af1a6e6250d581
Nacho <carlos.ralcantar@alumnos.udg.mx>
2017-12-05 07:10:52 -0600

    Seccion noticias terminado

--- Proyecto/platforms/browser/www/js/inicioAlumno.js
+++ Proyecto/platforms/browser/www/js/inicioAlumno.js
@@ -35,7 +35,7 @@ function cargarBotonDesmatricular(){
                 location.href="index.html";
             }
             else{
-                alert("Intenta más tarde")
+                alert("Intenta más tarde");
             }
         })
     });
@@ -46,15 +46,104 @@ function cargarBotonDesmatricular(){
 
 function cargarNoticias(){
 
+$.post("https://proyectoppi.000webhostapp.com/proyecto/getPreviewNews.php", {idx: 0}, function(result,status){
+    //console.log(result);
+    var spt = result.split("¬");
+    document.getElementById("t0").innerHTML=spt[0];
+    document.getElementById("n0").innerHTML=spt[1];
+    document.getElementById("t1").innerHTML=spt[2];
+    document.getElementById("n1").innerHTML=spt[3];
+    document.getElementById("t2").innerHTML=spt[4];
+    document.getElementById("n2").innerHTML=spt[5];
+    document.getElementById("t3").innerHTML=spt[6];
+    document.getElementById("n3").innerHTML=spt[7];
+    document.getElementById("t4").innerHTML=spt[8];
+    document.getElementById("n4").innerHTML=spt[9];
+});
+
 $("#d0").click(function(){
-	//console.log("d0 pressed");
-	$.post("https://proyectoppi.000webhostapp.com/proyecto/getNews.php", {idx: 0}, function(result,status){
-		//console.log(result);
-		var splited = result.split("¬");
-		localStorage.setItem("splitted",splitted);
-		$(location).attr("href","news.html");
-		//console.log(splited.length)
-	});
+    //console.log("d0 pressed");
+    $.post("https://proyectoppi.000webhostapp.com/proyecto/getNews.php", {idx: 0}, function(result,status){
+        //console.log(result);
+        var splited = result.split("¬");
+        /*if(splited.length >1)
+            alert("Error al cargar");
+        else {*/
+            localStorage.setItem("foto", splited[0]);
+            localStorage.setItem("titulo", splited[1]);
+            localStorage.setItem("fecha", splited[2]);
+            localStorage.setItem("nota", splited[3]);
+            $(location).attr("href","news.html");
+        //}
+    });
+});
+
+$("#d1").click(function(){
+    //console.log("d0 pressed");
+    $.post("https://proyectoppi.000webhostapp.com/proyecto/getNews.php", {idx: 1}, function(result,status){
+        //console.log(result);
+        var splited = result.split("¬");
+        /*if(splited.length >1)
+            alert("Error al cargar");
+        else {*/
+            localStorage.setItem("foto", splited[0]);
+            localStorage.setItem("titulo", splited[1]);
+            localStorage.setItem("fecha", splited[2]);
+            localStorage.setItem("nota", splited[3]);
+            $(location).attr("href","news.html");
+        //}
+    });
+});
+
+$("#d2").click(function(){
+    //console.log("d0 pressed");
+    $.post("https://proyectoppi.000webhostapp.com/proyecto/getNews.php", {idx: 2}, function(result,status){
+        //console.log(result);
+        var splited = result.split("¬");
+        /*if(splited.length < 1)
+            alert("Error al cargar");
+        else {*/
+            localStorage.setItem("foto", splited[0]);
+            localStorage.setItem("titulo", splited[1]);
+            localStorage.setItem("fecha", splited[2]);
+            localStorage.setItem("nota", splited[3]);
+            $(location).attr("href","news.html");
+        //}
+    });
+});
+
+$("#d3").click(function(){
+    //console.log("d0 pressed");
+    $.post("https://proyectoppi.000webhostapp.com/proyecto/getNews.php", {idx: 3}, function(result,status){
+        //console.log(result);
+        var splited = result.split("¬");
+        /*if(splited.length >1)
+            alert("Error al cargar");
+        else {*/
+            localStorage.setItem("foto", splited[0]);
+            localStorage.setItem("titulo", splited[1]);
+            localStorage.setItem("fecha", splited[2]);
+            localStorage.setItem("nota", splited[3]);
+            $(location).attr("href","news.html");
+        //}
+    });
+});
+
+$("#d4").click(function(){
+    //console.log("d0 pressed");
+    $.post("https://proyectoppi.000webhostapp.com/proyecto/getNews.php", {idx: 4}, function(result,status){
+        //console.log(result);
+        var splited = result.split("¬");
+        /*if(splited.length >1)
+            alert("Error al cargar");
+        else {*/
+            localStorage.setItem("foto", splited[0]);
+            localStorage.setItem("titulo", splited[1]);
+            localStorage.setItem("fecha", splited[2]);
+            localStorage.setItem("nota", splited[3]);
+            $(location).attr("href","news.html");
+        //}
+    });
 });
     
 }
--- Proyecto/platforms/browser/www/js/news.js
+++ Proyecto/platforms/browser/www/js/news.js
@@ -4,3 +4,8 @@ document.getElementById("titulo").innerHTML = splitted[0];
 document.getElementById("fecha").innerHTML = splitted[1];
 document.getElementById("nota").innerHTML = splitted[2];*/
 //document.getElementById("titulo").innerHTML = splitted[3];
+
+$("#foto").append("<img src="+localStorage.getItem("foto")+" style='width=300px'>");
+document.getElementById("titulo").innerHTML=localStorage.getItem("titulo");
+document.getElementById("fecha").innerHTML=localStorage.getItem("fecha");
+document.getElementById("nota").innerHTML=localStorage.getItem("nota");
