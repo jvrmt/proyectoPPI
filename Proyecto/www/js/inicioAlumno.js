e4a1448b18861f21ed0fbabcf49f99df0210a912
Nacho <carlos.ralcantar@alumnos.udg.mx>
2017-12-04 23:12:46 -0600

    Agregado de seccion de noticias

--- Proyecto/www/js/inicioAlumno.js
+++ Proyecto/www/js/inicioAlumno.js
@@ -7,3 +7,15 @@
 });*/
 
 document.getElementById("temp").innerHTML='<object type="text/html" data="https://crollz.000webhostapp.com/jsonDate/jsonDate.php" ></object>';
+//document.getElementById("news").innerHTML='<object type="text/html" data="news.html" ></object>';
+
+$("#d0").click(function(){
+	//console.log("d0 pressed");
+	$.post("https://proyectoppi.000webhostapp.com/proyecto/getNews.php", {idx: 0}, function(result,status){
+		//console.log(result);
+		var splited = result.split("¬");
+		localStorage.setItem("splitted",splitted);
+		$(location).attr("href","news.html");
+		//console.log(splited.length)
+	});
+});
---
+++ Proyecto/www/js/news.js
@@ -0,0 +1,6 @@
+/*var splitted = localStorage.getItem("splitted");
+
+document.getElementById("titulo").innerHTML = splitted[0];
+document.getElementById("fecha").innerHTML = splitted[1];
+document.getElementById("nota").innerHTML = splitted[2];*/
+//document.getElementById("titulo").innerHTML = splitted[3];
