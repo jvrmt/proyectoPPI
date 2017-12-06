814a511e0ce7f88fd82ca23270f2ad10ee94a08b
Javier <javier.moreno@alumnos.udg.mx>
2017-12-05 22:05:45 -0600

    Icono y corrección de charts

--- Proyecto/www/js/charts.js
+++ Proyecto/www/js/charts.js
@@ -1,5 +1,6 @@
 function verChart(asistencias,faltas) 
 {
+    /*
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);
 
@@ -12,7 +13,13 @@ function verChart(asistencias,faltas)
         var options = {
             title: "Promedio de asistencias"
         };
+        //var opcion=JSON.parse(options);
         var chart = new google.visualization.PieChart(document.getElementById('visorChart'));
-        chart.draw(data, options);
-    }   
+        chart.draw(data, opcion);
+    } */
+    var total=parseInt(faltas)+parseInt(asistencias);
+    document.getElementById("Faltas").innerHTML="Faltas: "+faltas+" ("+Math.round((100/total)*parseInt(faltas))+"%)";
+    document.getElementById("Asistencias").innerHTML="Asistencias: "+asistencias+" ("+Math.round((100/total)*parseInt(asistencias))+"%)";
+    document.getElementById("imagenChart").src="https://chart.googleapis.com/chart?cht=p3&chd=t:"+asistencias+","+faltas+"&chs=300x150&chl=Asistencias|Faltas"
+    
 }
